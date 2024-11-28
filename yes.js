c4d38747ff32ee49b5c9c84d1d18c94659d0d795
LizFam <jessica.m.w.lizarraga@gmail.com>
2024-11-28 06:21:13 +0000

    Create yes.js

---
+++ yes.js
@@ -0,0 +1,9 @@
+<script>
+function clickAction() {
+  result.textContent = 'You're terrible.';
+  if (correct.checked) {
+    result.textContent = 'Of course, the only correct response is yes';
+  }
+}
+button.onclick = clickAction;
+</script>
