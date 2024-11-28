e54e230c0616a7459258a94c190a35b66e978049
LizFam <jessica.m.w.lizarraga@gmail.com>
2024-11-28 18:10:15 +1100

    Update yes.js

--- yes.js
+++ yes.js
@@ -3,7 +3,7 @@ function clickAction() {
   result.textContent = 'You're terrible.';
   if (correct.checked) {
     result.textContent = 'Of course, the only correct response is yes';
-  }
+  }button.onclick = clickAction;
 }
-button.onclick = clickAction;
+
 </script>
